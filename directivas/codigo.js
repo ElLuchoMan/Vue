c6c507c1daef0519f6c5a8241f16bfe5566c6114
ElLuchoMan <luchoman96@gmail.com>
2021-03-09 19:50:27 -0500

    uso de v-for para renderizar listas y listas de objetos

--- directivas/codigo.js
+++ directivas/codigo.js
@@ -1,7 +1,13 @@
 const app = new Vue({
     el: '#main',
     data: {
-     conectado: true,
-     edad:15,
+        conectado: true,
+        edad: 15,
+        lista: ['Diana', 'Luis', 'Tobby'],
+        listaPersonas: [
+            { nombre: 'Diana', edad: 19 },
+            { nombre: 'Luis', edad: 24 },
+            { nombre: 'Tobby', edad: 1 }
+        ],
     }
 });
